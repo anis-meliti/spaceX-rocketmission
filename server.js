417cb7d41bf850c5135495f2ce95b998067e84b6
Anis Melliti <ansmeliti@gmail.com>
2019-09-29 23:36:18 +0100

    make the project ready for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const graphqlHTTP = require('express-graphql');
 const app = express();
 const cors = require('cors');
 const schema = require('./schema');
+const path = require('path');
 
 // allow cross-origin
 app.use(cors());
@@ -13,7 +14,10 @@ app.use(
     graphiql: true
   })
 );
-
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 PORT = process.env.PORT || 5000;
 app.listen(PORT, err =>
   err ? console.error(err) : console.log(`server is running on port ${PORT}`)
